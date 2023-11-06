4e011845bb37c80200703d9fbdd02eea09d22c17
Projeto-Automation <148788352+Projeto-Automation@users.noreply.github.com>
2023-11-06 16:51:17 -0300

    Add files via upload

---
+++ network/subnet/README.md
@@ -0,0 +1,14 @@
+# MODULOS-AZURE
+
+### MODULO CREATE SUBNET
+```
+module "subnet" {
+  source                = "github.com/Projeto-Automation/MODULOS-AZURE/network//subnet"
+  
+  name                  = ""
+  resource_group_name   = ""
+  virtual_network_name  = ""
+  ip_subnet             = ""
+  security_group_id     = ""
+}
+```
---
+++ network/virtual_network_subnet/variable.tf
@@ -0,0 +1,25 @@
+##  ----------  ( Documentação Variaveis )  ----------  ##
+
+#  ----  Variaveis MAIN
+
+variable "resource_group_name" {
+    type = string  
+}
+
+#  ----  Variaveis NETWORK
+
+variable "virtual_network_name" {
+    type = string  
+}
+
+variable "ip_network_virtual" {
+    type = string  
+}
+
+variable "subnet_name" {
+    type = string  
+}
+
+variable "ip_subnet" {
+    type = string  
+}
