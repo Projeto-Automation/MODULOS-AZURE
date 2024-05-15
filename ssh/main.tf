0734453d0f02210c10b846befb270af41a3cb205
Projeto-Automation <148788352+Projeto-Automation@users.noreply.github.com>
2024-05-15 08:46:25 -0300

    Update main.tf

--- ssh/main.tf
+++ ssh/main.tf
@@ -1,7 +1,12 @@
-data "vault_generic_secret" "secrets" {
+data "vault_generic_secret" "variaveis_ambiente_teste" {
+    path = "guacamole/variaveis_ambiente"
+}
+
+data "vault_generic_secret" "teste_secrets" {
     path = var.path_vault
 }
-resource "guacamole_connection_ssh" "connection_ssh" {
+
+resource "guacamole_connection_ssh" "teste_connection_ssh" {
     name = var.nome_vm
     parent_identifier = var.nome_raiz
     attributes {
@@ -9,12 +14,12 @@ resource "guacamole_connection_ssh" "connection_ssh" {
         guacd_port      = var.guacd_port
     }
     parameters {
-        hostname = data.vault_generic_secret.secrets.data["hostname"]
-        username = data.vault_generic_secret.secrets.data["username"]
-        password = data.vault_generic_secret.secrets.data["password"]
-        port     = data.vault_generic_secret.secrets.data["port"]
-        recording_path = data.vault_generic_secret.variaveis_ambiente.data["path_record"]
-        recording_name = data.vault_generic_secret.variaveis_ambiente.data["name_record"]
+        hostname = data.vault_generic_secret.teste_secrets.data["hostname"]
+        username = data.vault_generic_secret.teste_secrets.data["username"]
+        password = data.vault_generic_secret.teste_secrets.data["password"]
+        port     = data.vault_generic_secret.teste_secrets.data["port"]
+        recording_path = data.vault_generic_secret.variaveis_ambiente_teste.data["path_record"]
+        recording_name = data.vault_generic_secret.variaveis_ambiente_teste.data["name_record"]
         sftp_enable = true
     }
 }
